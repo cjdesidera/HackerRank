236b7921403ef85f17779db253ef6f21452fa451
Cesar Desiderá <108702707+cjdesidera@users.noreply.github.com>
2022-11-30 10:54:38 -0300

    Create Python_If_Else.py

---
+++ Python/Introduction/Python_If_Else.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Se for impar, imprima Estranho
+# Se for par e estiver no intervalo inclusivo de 2 a 5 , imprima Not Weird
+# Se for par e estiver no intervalo inclusivo de 6 a 20 , imprima Weird
+# Se for par e maior que 20, imprima Not Weird
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n >= 1 and n <= 100:
+        if n % 2 == 0:
+            if n > 20:
+                print('Not Weird')
+            else:
+                if n >= 6:
+                    print('Weird')
+                else:
+                    print('Not Weird')
+        else:
+            print('Weird')
